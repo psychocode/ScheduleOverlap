f369106c16f9a9db51a40dbfc05bfd913546a081
Cody Geest <cody-geest@uiowa.edu>
2014-12-26 10:48:41 -0600

    Added overlap detection

--- js/main.js
+++ js/main.js
@@ -1,33 +1,72 @@
 /**
  * Created by Cody on 12/25/2014.
  */
-
-function mainController() {
-    this.matcher = /^([\/\s\w]+).+[.](.+)/mg;
+function MainController() {
+    this.matcher = /^\W*([\/\s\w]+\w)[\W\s-.]+(\d+:\d+\w*[\W\s-.]+\d+:\d+\w*)/mg;
     this.matches = [];
     this.match = function(str) {
         var m;
         this.matches = [];
-        while ((m = this.matcher.exec(str)) != null) {
+        var times;
+        var start;
+        var end;
+        while ((m = this.matcher.exec(str)) !== null) {
             if (m.index === this.matcher.lastIndex) {
                 this.matcher.lastIndex++;
             }
-            var times = m[2].split("-");
-            this.matches.push({name: m[1], start: times[0], end: times[1]});
+
+            times = m[2].replace(" ", "").split("-");
+            start = this.getTimeNumber(times[0]);
+            end = this.getTimeNumber(times[1]);
+
+            if (start > 1800 && end < 200) end += 2400;
+            if (end < start) end += 1200;
+
+
+            this.matches.push(
+                {
+                    name: m[1],
+                    string: m[1] + " " + times[0] + " - " + times[1],
+                    start: start,
+                    end: end
+                }
+            );
+
         }
+    };
+
+    this.getTimeNumber = function (str) {
+        var time = str.replace(/(am)/gi, "");
+        var isPm = false;
+        time = time.replace(":", "");
+        if (time.toLowerCase().indexOf("pm") > -1)
+        {
+            time = time.replace(/(pm)/gi, "");
+            isPm = true;
+        }
+        var rawTimeNumber = Number(time);
+        return isPm ? rawTimeNumber + 1200 : rawTimeNumber;
+    };
+
+    this.checkOverlap = function (match)
+    {
+        var otherMatches =_.without(this.matches, match);
+        return _.reduce(otherMatches, function (memo, obj) {
+            var overlaps = (match.start >= obj.start && match.start <= obj.end) ||
+                (obj.start >= match.start && obj.start <= match.end);
+
+            return (memo) ? (memo) : (overlaps);
+        }, false);
     };
 
     this.getOffsetPercent = function (match)
     {
-        var start = match.start.replace(":", "");
-        return Number(start) / 2400 * 100;
+        return match.start / 2510 * 100;
     };
 
     this.getTimePercent = function(match)
     {
-        var start = Number(match.start.replace(":", ""));
-        var end = Number(match.end.replace(":", ""));
-        return (end-start) /2400 * 100;
+        return (match.end-match.start) / 2510 * 100;
     };
 
 
@@ -37,5 +76,5 @@ function mainController() {
 var app = angular.module("app", []);
 
 app.controller("main", [function() {
-    return new mainController();
+    return new MainController();
 }]);
